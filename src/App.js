6a6522037d043898bd6a3c1eeb1d1453b7db604a
emmanuellar <emmanuellar805@gmail.com>
2020-02-24 16:38:57 +0100

    updated my app with  shop page, a preview collection and a collection item components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component';
+import shopPage from './pages/shop/shop.component'; 
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={shopPage} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+// onClick={() => props.history.push('/')} is also used for routing in React
